32c81a24214cec1d407c16c8097599df57ad7357
Naveen Bhatta <bhatta.naveen@gmail.com>
2018-12-04 09:07:28 +0000

    Recipe to install required packages

--- docker/recipes/install-req-package.rb
+++ docker/recipes/install-req-package.rb
@@ -3,3 +3,8 @@
 # Recipe:: install-req-package
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+['yum-utils','device-mapper-persistent-data','lvm2'].each do |p|
+  package p do
+    action :install
+  end
+end
