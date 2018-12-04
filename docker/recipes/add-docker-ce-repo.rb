e83cf2d41a88be881c68658825da28bf2f652ceb
Naveen Bhatta <bhatta.naveen@gmail.com>
2018-12-04 09:36:49 +0000

    Download Docker CE Repo File

--- docker/recipes/add-docker-ce-repo.rb
+++ docker/recipes/add-docker-ce-repo.rb
@@ -3,3 +3,7 @@
 # Recipe:: add-docker-ce-repo
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+remote_file '/etc/yum.repos.d/docker-ce.repo' do
+  source 'https://download.docker.com/linux/centos/docker-ce.repo'
+  action :create
+end
